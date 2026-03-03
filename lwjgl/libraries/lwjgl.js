922523f9e2d985b14e0156c8b90a4f36e6edb7f3
KyleKart <71104484+KyleKart@users.noreply.github.com>
2026-03-03 14:47:29 -0400

    fgdf

--- lwjgl/libraries/lwjgl.js
+++ lwjgl/libraries/lwjgl.js
@@ -1,4 +1,4 @@
-var r = await fetch('/lwjgl/libraries/liblwjgl.so');
+var r = await fetch('/cheerpj/lwjgl/libraries/liblwjgl.so');
 var buf = await r.arrayBuffer();
 export default {
 	wasmModule: buf
